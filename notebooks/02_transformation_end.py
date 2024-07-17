7b2316c43abc090ed3c5c8c78ef0471e37b06004
fernandotedokon <fernandotedokon>
2024-07-17 16:00:13 +0000

    Transformation refactor end

--- notebooks/02_transformation_end.py
+++ notebooks/02_transformation_end.py
@@ -1,6 +1,11 @@
 # Databricks notebook source
+from pyspark.sql.functions import to_date, first, col, round
+
+
+# COMMAND ----------
+
 df_junto = spark.read.parquet("dbfs:/databricks-results/01_transformation_initial/*/*/*")
-df_junto.show(5)
+
 
 # COMMAND ----------
 
@@ -11,36 +16,26 @@ df_junto.select("moeda").distinct().count()
 moedas = ['USD', 'EUR', 'GBP']
 
 df_moedas = df_junto.filter(df_junto.moeda.isin(moedas))
-df_moedas.show()
+
 
 # COMMAND ----------
-
-from pyspark.sql.functions import to_date
 
 df_moedas = df_moedas.withColumn("data", to_date("data"))
 
 # COMMAND ----------
 
-df_moedas.printSchema()
-
-# COMMAND ----------
-
-from pyspark.sql.functions import first
-
 resultado_taxas_conversao = df_moedas.groupBy("data") \
            .pivot("moeda") \
            .agg(first("taxa")) \
                
-resultado_taxas_conversao.show(5)
+
 
 # COMMAND ----------
 
 resultado_valores_reais = resultado_taxas_conversao.select("*")
-resultado_valores_reais.show(5)
+
 
 # COMMAND ----------
-
-from pyspark.sql. functions import col, round
 
 for moeda in moedas: 
         resultado_valores_reais = resultado_valores_reais\
@@ -48,10 +43,6 @@ for moeda in moedas:
                                                                         moeda, round(1/col(moeda), 4)
                                                                 )
 
-
-# COMMAND ----------
-
-resultado_valores_reais.show()
 
 # COMMAND ----------
 
@@ -73,11 +64,3 @@ resultado_valores_reais.write\
     .option("header", "true")\
     .save("dbfs:/databricks-results/02_transformation_end/valores_reais")
 
-
-# COMMAND ----------
-
-display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_reais/"))
-
-# COMMAND ----------
-
-display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/taxas_conversao/"))
