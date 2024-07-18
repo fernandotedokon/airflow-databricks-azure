74e82b413012339c5a2fe6a3bad2ce51dc19c689
fernandotedokon <fernandotedokon>
2024-07-18 19:53:36 +0000

    Ajuste envio

--- notebooks/03_automate_reporting.py
+++ notebooks/03_automate_reporting.py
@@ -1,19 +1,12 @@
 # Databricks notebook source
-<<<<<<< Updated upstream
-%pip install kaleido slack-sdk
-=======
 # MAGIC %pip install kaleido slack-sdk
 
 # COMMAND ----------
->>>>>>> Stashed changes
 
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-<<<<<<< Updated upstream
 import pyspark.pandas as ps
-=======
->>>>>>> Stashed changes
 
 
 # COMMAND ----------
@@ -30,10 +23,6 @@ display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_re
 
 nome_arquivo = dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_reais/")[-1].name
 
-<<<<<<< Updated upstream
-display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_reais/"))
-
-=======
 
 # COMMAND ----------
 
@@ -41,42 +30,33 @@ display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_re
 
 # COMMAND ----------
 
->>>>>>> Stashed changes
 display(nome_arquivo)
 
 # COMMAND ----------
 
 path = "/dbfs/databricks-results/02_transformation_end/valores_reais/" + nome_arquivo
 
-<<<<<<< Updated upstream
-# Salvando arquivo de taxas
-=======
+
 # COMMAND ----------
 
->>>>>>> Stashed changes
+display(path)
+
+# COMMAND ----------
+
 try:
-    response = client.files_upload_v2(
-        channel='C07CG2SV2KZ',
+    enviando_arquivo_csv = client.files_upload_v2(
+        channel="C07CG2SV2KZ",  
         title="Arquivo no formato CSV do valor do real convertido",
         file=path,
         filename="valores_reais.csv",
         initial_comment="Segue anexo o arquivo CSV:",
     )
-    assert response["file"]  # the uploaded file
 except SlackApiError as e:
     # You will get a SlackApiError if "ok" is False
     assert e.response["ok"] is False
     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
     print(f"Got an error: {e.response['error']}")
 
-<<<<<<< Updated upstream
-	
-=======
-# COMMAND ----------
-
-import pyspark.pandas as ps
-
->>>>>>> Stashed changes
 # COMMAND ----------
 
 ps.read_csv("dbfs:/databricks-results/02_transformation_end/valores_reais/").head()
@@ -86,12 +66,11 @@ ps.read_csv("dbfs:/databricks-results/02_transformation_end/valores_reais/").hea
 df_valores_reais = ps.read_csv("dbfs:/databricks-results/02_transformation_end/valores_reais/")
 df_valores_reais.head()
 
-<<<<<<< Updated upstream
+
 # COMMAND ----------
 
 df_valores_reais.plot.line(x="data", y='USD')
-=======
->>>>>>> Stashed changes
+
 
 # COMMAND ----------
 
@@ -109,19 +88,22 @@ for moeda in df_valores_reais.columns[1:]:
 
 # COMMAND ----------
 
-def enviando_imagens(moeda_cotacao):
+!ls ./imagens
 
+# COMMAND ----------
+
+def enviando_imagens(moeda_cotacao):
     enviando_imagens = client.files_upload_v2(
         channel='C07CG2SV2KZ',
         title="Arquivo imagens de cotacoes",
         file=f"./imagens/{moeda_cotacao}.png"
     )
-<<<<<<< Updated upstream
-	
-=======
 
 
->>>>>>> Stashed changes
+# COMMAND ----------
+
+display(moeda)
+
 # COMMAND ----------
 
 for moeda in df_valores_reais.columns[1:]:
