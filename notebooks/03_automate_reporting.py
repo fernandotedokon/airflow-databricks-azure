129b36064ba1e28990f790d555734ff8ceed4062
fernandotedokon <fernandotedokon>
2024-07-18 18:59:22 +0000

    Initial

--- notebooks/03_automate_reporting.py
+++ notebooks/03_automate_reporting.py
@@ -1,10 +1,19 @@
 # Databricks notebook source
+<<<<<<< Updated upstream
 %pip install kaleido slack-sdk
+=======
+# MAGIC %pip install kaleido slack-sdk
+
+# COMMAND ----------
+>>>>>>> Stashed changes
 
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+<<<<<<< Updated upstream
 import pyspark.pandas as ps
+=======
+>>>>>>> Stashed changes
 
 
 # COMMAND ----------
@@ -21,15 +30,30 @@ display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_re
 
 nome_arquivo = dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_reais/")[-1].name
 
+<<<<<<< Updated upstream
 display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_reais/"))
 
+=======
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/databricks-results/02_transformation_end/valores_reais/"))
+
+# COMMAND ----------
+
+>>>>>>> Stashed changes
 display(nome_arquivo)
 
 # COMMAND ----------
 
 path = "/dbfs/databricks-results/02_transformation_end/valores_reais/" + nome_arquivo
 
+<<<<<<< Updated upstream
 # Salvando arquivo de taxas
+=======
+# COMMAND ----------
+
+>>>>>>> Stashed changes
 try:
     response = client.files_upload_v2(
         channel='C07CG2SV2KZ',
@@ -45,7 +69,14 @@ except SlackApiError as e:
     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
     print(f"Got an error: {e.response['error']}")
 
+<<<<<<< Updated upstream
 	
+=======
+# COMMAND ----------
+
+import pyspark.pandas as ps
+
+>>>>>>> Stashed changes
 # COMMAND ----------
 
 ps.read_csv("dbfs:/databricks-results/02_transformation_end/valores_reais/").head()
@@ -55,9 +86,12 @@ ps.read_csv("dbfs:/databricks-results/02_transformation_end/valores_reais/").hea
 df_valores_reais = ps.read_csv("dbfs:/databricks-results/02_transformation_end/valores_reais/")
 df_valores_reais.head()
 
+<<<<<<< Updated upstream
 # COMMAND ----------
 
 df_valores_reais.plot.line(x="data", y='USD')
+=======
+>>>>>>> Stashed changes
 
 # COMMAND ----------
 
@@ -82,7 +116,12 @@ def enviando_imagens(moeda_cotacao):
         title="Arquivo imagens de cotacoes",
         file=f"./imagens/{moeda_cotacao}.png"
     )
+<<<<<<< Updated upstream
 	
+=======
+
+
+>>>>>>> Stashed changes
 # COMMAND ----------
 
 for moeda in df_valores_reais.columns[1:]:
